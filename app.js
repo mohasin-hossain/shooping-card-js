cb58b75fc2ad4879be10e1dc4eacaeca8e500ab9
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-08-21 10:45:22 +0600

    UpdateProductNumber function to handle both phone and case events

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-input');
-    let caseValue = parseInt(caseInput.value);
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-input');
+    let productValue = parseInt(productInput.value);
    
     if(isIncreasing == true) {
-        caseValue = caseValue + 1;
-    } else if (caseValue > 0) {
-        caseValue = caseValue - 1;
+        productValue = productValue + 1;
+    } else if (productValue > 0) {
+        productValue = productValue - 1;
     }
 
-    caseInput.value = caseValue;
+    productInput.value = productValue;
 
     // Update Case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseValue * 59;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productValue * price;
 }
 
+// Handle phone increase decreasse Events
+document.getElementById('phone-plus').addEventListener('click', function() {
+    updateProductNumber('phone', 1250, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function() {
+    updateProductNumber('phone', 1250, false);
+})
+
+// handle case increase decrease Events
 document.getElementById('case-plus').addEventListener('click', function() {
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 })
 
 document.getElementById('case-minus').addEventListener('click', function() {
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 })
