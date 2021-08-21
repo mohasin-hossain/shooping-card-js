5559d637630e9f81fee3ea72b4db61b872b41b71
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-08-21 10:11:48 +0600

    Case number update function js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-input');
+    let caseValue = parseInt(caseInput.value);
+   
+    if(isIncreasing == true) {
+        caseValue = caseValue + 1;
+    } else if (caseValue > 0) {
+        caseValue = caseValue - 1;
+    }
+
+    caseInput.value = caseValue;
+
+    // Update Case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseValue * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function() {
+    updateCaseNumber(true);
+})
+
+document.getElementById('case-minus').addEventListener('click', function() {
+    updateCaseNumber(false);
+})
